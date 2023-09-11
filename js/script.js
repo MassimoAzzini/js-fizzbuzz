f1da218e8eca9ea8fd5e994ed53035bb00c0e8e4
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-11 17:35:00 +0200

    [edit]: JS

--- js/script.js
+++ js/script.js
@@ -2,27 +2,30 @@
 const containerBox = document.querySelector('.content')
 
 // creo un ciclo per creare la numerazione da 1 a 100 e creo il div con classe box per tutti
-for(let i = 0; i < 100; i++){
+for(let i = 1; i < 100; i++){
   let box = document.createElement('div');
-  console.log(i + 1);
+  console.log(i);
   box.classList.add('box');
-  box.append(i + 1);
 
   // creo delle condizioni per i multipli di 3 & 5, di 3, di 5 in questo ordine per dare priorita alla classe 3 & 5 
-  if((!((i + 1) % 3)) & (!((i + 1) % 5))){
+  if((i % 3 == 0) & (i % 5 == 0)){
     
     box.classList.add('fizz-buzz');
     box.innerHTML = ('fizzbuzz');
 
-  }else if(!((i + 1) % 3)){
+  }else if(i % 3 == 0){
 
     box.classList.add('fizz');
     box.innerHTML = ('fizz');
 
-  }else if(!((i + 1) % 5)){
+  }else if(i % 5 == 0){
 
     box.classList.add('buzz');
     box.innerHTML = ('buzz');
+
+  }else{
+
+    box.append(i);
 
   }
 
